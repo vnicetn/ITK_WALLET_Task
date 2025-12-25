05ec65aaa4487a9fb63cfbb338431b32d1c90547
vnicetn <vincentandpancakes@gmail.com>
2025-12-25 05:50:37 -0800

    add CHECK constraint for the uint model field

--- migrations/0001_wallet.up.sql
+++ migrations/0001_wallet.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS wallets (
 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-balance INT NOT NULL DEFAULT 0,
+balance INT NOT NULL DEFAULT 0 CHECK (balance >= 0),
 created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
